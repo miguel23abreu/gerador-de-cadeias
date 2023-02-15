6b0209151a84934ffd192cfeebd3e3a8d51899a4
miguel abreu <guelabreu@hotmail.com>
2023-02-14 23:24:36 -0300

    first commit

---
+++ ExpressaoReg.js
@@ -0,0 +1,36 @@
+function Cadeia(expr){
+    let len = expr.lenght;
+    let exprA = new Array();
+    let exprV = new Array();
+    let exprP = new Array();
+    let contV = 0;
+    let contM = 0;
+    for(var i = len-1; i >= 0; i--){
+        if(expr[i] == '*'){
+            if(expr[i-1] != ')'){
+                exprV[contV] = expr[i-1];
+                expoA[i] = exp 
+                contV++;
+            }
+            else{
+                let p = i-1;
+                let c = 0;
+                while(expr[p] != '('){
+                    exprP[]
+                }
+            }
+        }
+    }
+}
+///////////////////////////////////////////////////////////////////////////////////////////////
+/*
+apenas uma forma de entender como juntar todos os elementos de vetor de caracteres em uma string
+    var strP = new Array();
+    var str = new Array();
+    var c = "M";
+    str.push("exemplo");
+
+    strP[0] = c;
+    strP[1] = str;
+    console.log(strP.join(''));
+    */
